94dd4092ff15f4c63af4af85b59d1eafb72d1596
Sancheza38 <93687081+Sancheza38@users.noreply.github.com>
2022-10-16 03:06:54 -0500

    testing errors
    i found alot of errors

--- app/src/main/java/com/example/mypantry/Recipies/RecipiesFragment.java
+++ app/src/main/java/com/example/mypantry/Recipies/RecipiesFragment.java
@@ -1,66 +1,68 @@
 package com.example.mypantry.Recipies;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.mypantry.Adapters.RandomRecipeAdapter;
+import com.example.mypantry.Listeners.RandomRecipeResponseListener;
+import com.example.mypantry.Models.RandomRecipeApiResponse;
 import com.example.mypantry.R;
+import com.example.mypantry.RequestManager;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RecipiesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class RecipiesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ProgressDialog dialog;
+    RequestManager manager;
+    RandomRecipeAdapter randomRecipeAdapter;
+    RecyclerView recyclerView;
 
     public RecipiesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RecipiesFragment newInstance(String param1, String param2) {
-        RecipiesFragment fragment = new RecipiesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        setHasOptionsMenu(true);
+
+        View view = inflater.inflate(R.layout.fragment_recipies, container,false);
+
+        dialog = new ProgressDialog(getActivity());
+        dialog.setTitle("Loading...");
+
+        manager = new RequestManager(getActivity());
+        manager.getRandomRecipes(randomRecipeResponseListener);
+        dialog.show();
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
+        @Override
+        public void didFetch(RandomRecipeApiResponse response, String message) {
+            //dialog.dismiss();
+            recyclerView = (RecyclerView) getView().findViewById(R.id.recycler_random);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 1));
+            randomRecipeAdapter = new RandomRecipeAdapter(getActivity(), response.recipes);
+            recyclerView.setAdapter(randomRecipeAdapter);
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipies, container, false);
-    }
+        @Override
+        public void didError(String message) {
+            Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+        }
+    };
+
 }
