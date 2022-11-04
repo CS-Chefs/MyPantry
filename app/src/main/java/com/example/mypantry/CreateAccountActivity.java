0c724be711d8841dbf57ad0341a86ac812601cea
Drahoenix <zpadalino@gmail.com>
2022-11-04 16:27:29 -0500

    updated some code in signup and signin

--- app/src/main/java/com/example/mypantry/CreateAccountActivity.java
+++ app/src/main/java/com/example/mypantry/CreateAccountActivity.java
@@ -2,13 +2,40 @@ package com.example.mypantry;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    TextView txtSignIn;
+    MaterialButton btnSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        txtSignIn = findViewById(R.id.txtSignIn);
+
+        txtSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        btnSignup = findViewById(R.id.btnSignUp);
+        btnSignup.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
